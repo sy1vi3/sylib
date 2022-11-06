04dffc4798ab6fa81b08bd08fc7de1860bd605d5
Sylvie <54775775+SylvieMayer@users.noreply.github.com>
2022-11-05 19:25:37 -0700

    Delete pros.h

--- include/pros.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "api.h"
-#include "vex/v5_api.h"
-#include "vex/v5_apitypes.h"
-#include "vex/v5_apiprivate.h"
-#include "vex/v5_apiuser.h"
-#include "vex/v5_color.h"
-#include "vex/v5_addrled.h"
